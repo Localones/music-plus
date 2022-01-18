68dd5ecd46eb1f71def0c8de8e47de92468196f5
Konstantin <kostya_na02>
2022-01-18 15:16:30 +0300

    slug update

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Link from "next/link";
-import styles from '../styles/Footer.module.css';
+import styles from '@/styles/Footer.module.css';
 
 function Footer() {
     return (
         <footer className={styles.footer}>
             <p>Copyright &copy; MusicPlus 2022</p>
             <p>
-                <Link href={`/`}>
+                <Link href={`/about`}>
                     <a>About This Project</a>
                 </Link>
             </p>
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,23 +1,29 @@
-import Layout from "../../components/Layout";
-import React from 'react';
-import {useRouter} from "next/router";
+import React from "react";
+import Layout from "@/components/Layout";
+import {API_URL} from "@/config/index";
+import EventItem from "@/components/EventItem";
 
-
-function IndexPage() {
-
-    const router = useRouter();
-
-    if (router.isFallback){
-        return <div>Loading...</div>
-    }
-
+export default function EventsPage({events}) {
     return (
-        <Layout title='Index'>
-            IndexPage
+        <Layout title='Home'>
+            <h1>Events</h1>
+            {events.length === 0 && <h3>No events to show</h3>}
+
+            {events.map(evt =>
+                <EventItem key={evt.id} evt={evt}/>
+            )}
         </Layout>
     )
 }
 
-export default IndexPage;
+export async function getStaticProps() {
+    const events = await fetch(`${API_URL}/api/events`)
+        .then(res => res.json());
 
-
+    return {
+        props: {
+            events,
+        },
+        revalidate: 1,
+    }
+}
